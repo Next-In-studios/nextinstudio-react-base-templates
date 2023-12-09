4933c2db2a54fac5a0680785eac43a179b3c9e92
Raju Ghorai <rajughorai41410@gmail.com>
2023-12-10 00:24:15 +0530

    update code in App.tsx

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './*.html'],
   darkMode: 'media',
   mode: 'jit',
+  colors: {
+      'color1': '#143a79',
+      'color2': '#e9f0fb',
+      'color3': '#ffd700',
+      'color4': '#fef6cb',
+      'color5': '#f2f2f2',
+      'color6': '#ffffff',
+    },
 };
